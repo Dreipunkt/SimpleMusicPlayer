df2561eaf0010055e3b417e98b82e3d24f27e125
Dreipunkt <crauc3@gmail.com>
2015-05-09 00:24:33 +0200

    added comments

--- myslider.cpp
+++ myslider.cpp
@@ -1,3 +1,25 @@
+/**
+SimpleMusicPlayer
+
+Copyright 2015 by Christian Rauch <crauc3@gmail.com>
+
+
+This file is part of SimpleMusicPlayer.
+
+SimpleMusicPlayer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SimpleMusicPlayer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SimpleMusicPlayer. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "myslider.h"
 
 MySlider::MySlider(QWidget *parent) :
@@ -19,13 +41,12 @@ void MySlider::mousePressEvent ( QMouseEvent * event )
     else
        newVal = minimum() + ((maximum()-minimum()) * event->x()) / width();
 
-    double halfHandleWidth = (0.5 * sr.width()) + 0.5; // Correct rounding
+    double halfHandleWidth = (0.5 * sr.width()) + 0.5;
     int adaptedPosX = event->x();
     if ( adaptedPosX < halfHandleWidth )
             adaptedPosX = halfHandleWidth;
     if ( adaptedPosX > width() - halfHandleWidth )
             adaptedPosX = width() - halfHandleWidth;
-    // get new dimensions accounting for slider handle width
     double newWidth = (width() - halfHandleWidth) - halfHandleWidth;
     double normalizedPosition = (adaptedPosX - halfHandleWidth)  / newWidth ;
 
--- myslider.h
+++ myslider.h
@@ -1,3 +1,25 @@
+/**
+SimpleMusicPlayer
+
+Copyright 2015 by Christian Rauch <crauc3@gmail.com>
+
+
+This file is part of SimpleMusicPlayer.
+
+SimpleMusicPlayer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SimpleMusicPlayer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SimpleMusicPlayer. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef MYSLIDER_H
 #define MYSLIDER_H
 
